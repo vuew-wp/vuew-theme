a6fb6ff2b3e7794441d556b02b26809934fd20ff
Jared Rethman <Jared.Rethman@24.com>
2018-04-04 00:47:56 +0200

    Added a chunked match version of wp list pluck

---
+++ includes/utilities.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * List chunk pluck
+ *
+ * Like wp_list_pluck() but with the ability to match chunks of data using a index array of items to
+ * pluck from associative array|object
+ *
+ * @param   $list
+ * @param   array $keys_to_plucks
+ *
+ * @return  mixed
+ *
+ * @since   0.0.1
+ */
+function vw_list_chunk_pluck( $list, $keys_to_plucks = [] ){
+
+	/** @var array $plucks */
+	$plucks = $reduced_fields = [];
+
+	//Negate early
+	if( ! isset( $keys_to_plucks[ 0 ] ) ){
+		return $list;
+	}
+
+	//Make keys to pluck an associative array
+	foreach( $keys_to_plucks as $pluck ){
+		$plucks[ $pluck ] = null;
+	}
+
+	//Match up the items from required
+	//keys and intersect with object|array passed.
+	foreach ( $list as $k => $value ) {
+		/** Added reduced array(s) to returned menus */
+		$reduced_fields[ $k ] = array_intersect_key( (array) $value, $plucks );
+	}
+
+	return $reduced_fields;
+
+}
