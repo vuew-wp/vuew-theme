b8270616622f0a37475db6ff4c62a1e79f0884f1
Jared Rethman <>
2018-05-03 20:30:51 +0200

    Moved taxonomy archive. Now extends from Archive.vue

---
+++ src/Archive/Taxonomy.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-if="queryData">
+        <h1 v-html="queryData.payload.name"></h1>
+        <div class="uk-block" v-html="queryData.payload.description"></div>
+        <Loop :query-data="queryData.payload"></Loop>
+    </div>
+</template>
+
+<script>
+    import Archive from "../Archive/Archive";
+
+    export default {
+        extends : Archive
+    };
+</script>
