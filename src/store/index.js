bc644744606226ea3877f203751c84951b6d28c2
Jared Rethman <>
2018-05-03 20:50:21 +0200

    Added root state index for bundling all the modules etc

---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/**
+ * Root store
+ * Manages and maintains root state management
+ */
+import * as state from './state'
+import { actions } from './actions'
+import { getters }  from './getters'
+import { mutations } from './mutations'
+
+/**
+ * Modules
+ */
+/** Maintains query state */
+import query from './modules/query'
+/** Maintains cache delivery between queries */
+import cache from './modules/cache'
+/** Displaying data delivery stacktrace */
+import notify from './modules/notify'
+
+Vue.use( Vuex );
+
+/**
+ * Initialise VUEX
+ * @type {Vuex.Store}
+ */
+export const store =  new Vuex.Store({
+    state,
+    actions,
+    getters,
+    mutations,
+    modules: {
+        query,
+        cache,
+        notify
+    },
+    strict: process.env.NODE_ENV !== 'production',
+    plugins: []
+});
