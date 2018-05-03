2593f06f4544aa00cc9411dbf783f7954aaf725c
Jared Rethman <>
2018-05-03 20:04:53 +0200

    Updated index.php and wrapped SSR in <noscript>

--- index.php
+++ index.php
@@ -11,34 +11,35 @@
 
 <body <?php body_class() ?> >
 
-<div id="content" class="site-content" style="display: none;">
-    <div class="uk-grid">
-		<?php
+<noscript>
+    <div id="content" class="site-content">
+        <div class="uk-grid">
+            <?php
 
-		if ( have_posts() ) {
+            if ( have_posts() ) {
 
-			if ( is_home() && ! is_front_page() ) {
-				echo '<h1>' . single_post_title( '', false ) . '</h1>';
-			}
+                if ( is_home() && ! is_front_page() ) {
+                    echo '<h1>' . single_post_title( '', false ) . '</h1>';
+                }
 
-			while ( have_posts() ) {
+                while ( have_posts() ) {
 
-				the_post();
+                    the_post();
 
-				if ( is_singular() ) {
-					the_title( '<h1>', '</h1>' );
-				} else {
-					the_title( '<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
-				}
+                    if ( is_singular() ) {
+                        the_title( '<h1>', '</h1>' );
+                    } else {
+                        the_title( '<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+                    }
 
-				the_content();
-			}
-		}
+                    the_content();
+                }
+            }
 
-		?>
+            ?>
+        </div>
     </div>
-</div>
-
+</noscript>
 <!-- APP - START -->
 <div id="vuew-app"></div>
 <!-- APP - END -->
