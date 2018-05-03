299520f330392080108ed52a98cc23c2daac75a5
Jared Rethman <>
2018-05-03 20:54:34 +0200

    Added 404 component for handling all un-resolved routes. So not a true 404 - this will change in the future.

---
+++ src/Templates/404.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="uk-grid uk-grid-large">
+        <h1>404</h1>
+        <div class="uk-width-1-1">URL: <strong>{{siteUrl + queryData.path}}</strong>, could not be resolved.</div>
+        <Search/>
+    </div>
+</template>
+
+<script>
+
+    import Search from '../Partials/Search';
+
+    export default {
+
+        props: [
+            'queryData'
+        ],
+
+        components: {
+            Search
+        },
+
+        data(){
+            return {
+                loaded:false,
+                siteUrl: Vuew.baseUrl,
+            }
+        }
+
+    };
+</script>
