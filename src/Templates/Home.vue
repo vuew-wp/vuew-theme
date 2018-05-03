b040dd225ddf2c69c150984f820774815ee4cf85
Jared Rethman <>
2018-05-03 20:55:16 +0200

    Added separate home components for static and non-static home pages

---
+++ src/Templates/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-if="queryData">
+        <h1>HOME</h1>
+        <h1>{{ queryData.title }}</h1>
+        <div class="uk-block" v-html="queryData.content"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props:[
+            'queryData',
+        ],
+
+        data(){
+            return {
+                loaded:false,
+                title: 'Loading...',
+            }
+        }
+
+    };
+</script>
---
+++ src/Templates/Index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-if="queryData">
+        <h1>Latest Posts</h1>
+        <Loop :query-data="queryData.payload"></Loop>
+    </div>
+</template>
+
+<script>
+    import Archive from "../Archive/Archive";
+
+    export default {
+        extends : Archive
+    };
+</script>
