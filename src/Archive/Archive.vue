8c3820db2678183e6efee72822104612b6ff1e84
Jared Rethman <>
2018-05-03 20:29:07 +0200

    Added Archive parent component to be inherited from

---
+++ src/Archive/Archive.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Archive Template</h1>
+</template>
+<script>
+
+    import Loop from "../Partials/Loop/Loop";
+    export default {
+
+        components: {
+            Loop
+        },
+        props:[
+            'queryData',
+        ]
+
+    };
+
+</script>
