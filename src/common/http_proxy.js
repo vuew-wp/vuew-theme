d367e2c8d1ce824f6bee655612297ff697ff95ed
Jared Rethman <>
2018-05-28 15:58:28 +0200

    Added additional HTTP const specific to VUEW related endpoints.

--- src/common/http_proxy.js
+++ src/common/http_proxy.js
@@ -7,3 +7,11 @@ export const HTTP = axios.create( {
         vq: 1
     }
 } );
+
+export const HTTP_Vuew = axios.create( {
+    baseURL: Vuew.restRoot + 'vuew/v2/',
+    headers: { 'X-WP-Nonce': Vuew.nonce },
+    params: {
+        vq: 1
+    }
+} );
