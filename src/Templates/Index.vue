b32f4f6d012e1147e62fd63a24dc94b79bd802b4
Jared Rethman <>
2018-05-28 17:45:38 +0200

    Updated Index component to extend from Archive.vue component.

--- src/Templates/Index.vue
+++ src/Templates/Index.vue
@@ -1,14 +1,8 @@
-<template>
-    <div v-if="queryData">
-        <h1>Latest Posts</h1>
-        <Loop :query-data="queryData.payload"></Loop>
-    </div>
-</template>
-
 <script>
     import Archive from "../Archive/Archive";
 
     export default {
         extends : Archive
     };
+
 </script>
