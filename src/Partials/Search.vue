b975f9978db3886afd393892eca6d93d6e8d171d
Jared Rethman <>
2018-05-28 17:17:42 +0200

    Added start of Search.vue component

---
+++ src/Partials/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <form>
+        <!--<label class="uk-form-label uk-width-1-1 uk-h2" for="search">Search</label>-->
+        <v-select v-model="selected" :options="['foo','bar']"></v-select>
+    </form>
+</template>
+
+<script>
+
+    import vSelect from 'vue-select'
+
+    export default {
+
+        props: [
+            'defaultValue'
+        ],
+
+        components:{
+            vSelect
+        },
+
+        data(){
+            return {
+                selected: null
+            }
+        }
+
+    };
+</script>
