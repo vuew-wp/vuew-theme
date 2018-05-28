e190888e37f33ee8676690a17dbbeb081abffc8e
Jared Rethman <>
2018-05-28 17:29:13 +0200

    Refactored initial Pagination.vue component. Separated LoadMore specific logic into independent partial, PaginationLoadMore.vue.

--- src/Partials/Pagination/Pagination.vue
+++ src/Partials/Pagination/Pagination.vue
@@ -1,57 +1,28 @@
 <template>
-    <div class="vw-pagination">x
-        <p>{{queriedObject.foundPosts}}/{{queriedObject.postCount}}</p>
-        {{offset}}
-        <Post v-if="!isPending && index > 4" v-for="( post, index ) in posts" :key="post.path" :post="post"></Post>
-        <button @click="queryPosts">Load More</button>
-    </div>
+    <component :is="type" :posts="posts" :is-pending="isPending" :post-count="postCount" :found-posts="foundPosts" :columns="columns" :offset="offset" :elem-type="elemType"></component>
 </template>
 <script>
 
-    import Post from "../Loop/Post";
+    import LoadMore from './PaginationLoadMore';
 
     export default {
         props: [
             'type',
-            'posts'
+            'posts',
+            'foundPosts',
+            'postCount',
+            'columns',
+            'offset',
+            'elemType'
         ],
-        data(){
-            return {
-                allPosts: {},
-                loadedPosts: {},
-                offset: Vuew.config.query.ppp
-            }
-        },
         components:{
-            Post
-        },
-        methods: {
-            queryPosts:function(){
-                this.$store.dispatch( 'query/addPostsToList' );
-                this.allPosts = this.$store.getters[ 'query/getPostList' ];
-
-                this.offset = this.offset + Vuew.config.query.ppp;
-
-                this.loadedPosts = this.$store.getters[ 'query/getPostList' ];
-
-                for (let post in this.allPosts) {
-                    if( post >= Vuew.config.query.ppp ) {
-                        this.$set(this.loadedPosts, post, this.allPosts[post])
-                    }
-                }
-                console.log(this.loadedPosts)
-            }
+            LoadMore
         },
         computed: {
-            queriedObject:function(){
-                return this.$store.getters[ 'query/getCurrentQueriedObject' ].payload;
-            },
-            /*loadedPosts:function(){
-                return this.$store.getters[ 'query/getPostList' ];
-            },*/
             isPending:function(){
                 return this.$store.getters[ 'query/paginationPending' ];
             }
         }
     };
+
 </script>
---
+++ src/Partials/Pagination/PaginationLoadMore.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+
+        <component :is="elemType" v-if="index > offset" v-for="( newPost, index ) in posts" :key="newPost.path" :post="newPost" :columns="columns">
+            <p v-html="'Thing I want to say'"></p>
+            <div class="uk-alert" v-if="index % columns === 0">THIS IS my alert</div>
+        </component>
+
+        <div v-if="isPending" v-for="i in ppp" :class="'uk-width-large-1' + columns" class="uk-margin-bottom">
+            <div class="vw-flicker-loading-post" :style="{ animationDelay : getRandomDelay() + 's' }">
+            </div>
+        </div>
+
+        <button v-if="postCount < foundPosts" @click="loadPosts" class="uk-button uk-button-primary uk-width-1-1">
+            {{ isPending ? 'Pending...' : 'Load More' }} / ( {{ postCount }} / {{ foundPosts }} )
+        </button>
+
+    </div>
+</template>
+
+<script>
+
+    /**
+     * @todo The loader effect doesn't work as desired on first load
+     * if ppp does not equal a multiple of column count.
+     */
+
+    import Post from "../Loop/Post";
+    import PostVuew from "../Loop/PostVuew";
+
+    export default {
+        components:{
+            Post,
+            PostVuew
+        },
+        props:[
+            'posts',
+            'foundPosts',
+            'postCount',
+            'isPending',
+            'columns',
+            'offset',
+            'elemType'
+        ],
+        data() {
+            return {
+                /**
+                 * @todo use column count variable here
+                 */
+                ///offset: ( Math.ceil( Vuew.config.query.ppp / 3 ) * 3 ) - 1,
+                ppp: Vuew.config.query.ppp
+            }
+        },
+        methods: {
+            loadPosts:function(){
+                this.$store.dispatch( 'query/addPostsToList' );
+            },
+            getRandomDelay: function(){
+
+                /** Range */
+                const min = 0;
+                const max = 5;
+
+                /** Either a + or - */
+                const plusOrMinus = Math.random() < 0.5 ? '-' : '+';
+                /** Generate random number between 0.00 and 3.00 */
+                const randumNum = ( Math.random() * ( max - min + 1 ) + min).toFixed(2);
+
+                return plusOrMinus + randumNum;
+
+            }
+        },
+        computed:{/** */}
+    }
+
+</script>
