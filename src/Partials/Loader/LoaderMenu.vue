6a037f647ad59ae42ff70ac20ac3bdd1578d2087
Jared Rethman <>
2018-05-28 17:18:23 +0200

    Individual Loader components, might use later

---
+++ src/Partials/Loader/LoaderMenu.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 style="color: red; background: white;">
+        Loading Component, what do you think?
+    </h1>
+</template>
+<script>
+    export default {
+        mounted:() => {
+            console.log("Loading Component")
+        }
+    }
+</script>
