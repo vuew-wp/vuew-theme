da4e3e40309c90fdd8a989a23f5a8d23bbbe2c98
Jared Rethman <>
2018-05-28 15:49:06 +0200

    REST class refactor, a lot more DRY

--- includes/classes/REST.php
+++ includes/classes/REST.php
@@ -27,6 +27,10 @@ class REST extends Factory {
 		'entry-category'
 	];
 
+	const API_NAMESPACE = 'vuew/v2';
+
+	protected static $date_format = '';
+
 	/**
 	 * REST constructor.
 	 */
@@ -41,7 +45,14 @@ class REST extends Factory {
 			add_action( 'rest_prepare_' . $tax, [ __CLASS__, 'intercept_rest_prepare' ], 10, 3 );
 		}
 
-		add_action( 'rest_prepare_post_type', [ __CLASS__, 'intercept_rest_prepare' ], 10, 3);
+		add_action( 'rest_prepare_post_type', [ __CLASS__, 'intercept_rest_prepare' ], 10, 3 );
+
+		static::$date_format = get_option( 'date_format' );
+
+		/**
+		 * Custom endpoints
+		 */
+		add_action( 'rest_api_init', array( __CLASS__, 'rest_endpoints' ), 10 );
 	}
 
 	/**
@@ -57,7 +68,7 @@ class REST extends Factory {
 	 *
 	 * @since   0.0.1
 	 */
-	static function intercept_rest_prepare( $data, $item, $request ){
+	static function intercept_rest_prepare( $data, $item, $request ) {
 
 		/** @var \WP_REST_Request $params */
 		$params = $request->get_params();
@@ -70,15 +81,15 @@ class REST extends Factory {
 		/** @var string $current_action */
 		$current_action = str_replace( 'rest_prepare_', '', current_action() );
 		/** If action is rest_prepare_{$post_type} */
-		if( in_array( $current_action, static::POST_TYPES ) ){
+		if ( in_array( $current_action, static::POST_TYPES ) ) {
 			return static::prepare_post_type( $data, $item, $request );
 		}
 		/** If action is rest_prepare_{$taxonomy} */
-		if( in_array( $current_action, static::TAXONOMIES ) ){
+		if ( in_array( $current_action, static::TAXONOMIES ) ) {
 			return static::prepare_taxonomy_archive( $data, $item, $request );
 		}
 		/** If action is rest_prepare_post_type */
-		if( 'post_type' === $current_action ){
+		if ( 'post_type' === $current_action ) {
 			return static::prepare_post_type_archive( $data, $item, $request );
 		}
 
@@ -110,6 +121,7 @@ class REST extends Factory {
 			'title',
 			'excerpt',
 			'content',
+			'date',
 			'featured_media'
 		];
 
@@ -119,28 +131,46 @@ class REST extends Factory {
 		foreach ( $fields_to_keep as $field ) {
 			/** Remove rendered from fields */
 			if ( in_array( $field, [ 'content', 'title', 'excerpt' ] ) ) {
+				if( $field === 'title' ){
+					$_data[ $field ] = $data->data[ $field ]['rendered'];
+					continue;
+				}
+				if( $field === 'excerpt' ){
+					$_data[ $field ] = always_excerpt( $data->data[ 'content' ]['rendered'], $data->data[ 'excerpt' ]['rendered'] );
+					continue;
+				}
 				$_data[ $field ] = $data->data[ $field ]['rendered'];
 				continue;
 			}
+
 			/**
 			 * Handle featured images
 			 */
 			if ( 'featured_media' === $field ) {
-				$featured_image = $featured_image_lrg = '';
+				$featured_image = $featured_image_lrg = $featured_image_med = '';
 				if ( ( $attachment_id = $data->data['featured_media'] ) > 0 ) {
-					$featured_image = wp_get_attachment_image_url( $attachment_id, 'thumbnail' );
+					$featured_image     = wp_get_attachment_image_url( $attachment_id, 'thumbnail' );
+					$featured_image_med = wp_get_attachment_image_url( $attachment_id, 'medium' );
 					$featured_image_lrg = wp_get_attachment_image_url( $attachment_id, 'large' );
 				}
 				$_data['featured_media']['thumbnail'] = $featured_image;
-				$_data['featured_media']['large'] = $featured_image_lrg;
+				$_data['featured_media']['medium']    = $featured_image_med;
+				$_data['featured_media']['large']     = $featured_image_lrg;
 				continue;
 			}
+
+			if( 'date' === $field ){
+				$new_date = date( static::$date_format, strtotime( $data->data[ $field ] ) );
+				$_data[ $field ] = $new_date;
+				continue;
+			}
+
 			$_data[ $field ] = $data->data[ $field ];
 		}
 
 		$_data['route'] = [
 			'type'        => 'post_type',
-			'rest_base'   => \Vuew\REST_BASES[ 'post_type' ][ $data->data['type'] ],
+			'rest_base'   => \Vuew\REST_BASES['post_type'][ $data->data['type'] ],
 			'type_value'  => $data->data['type'],
 			'breadcrumbs' => vw_make_bread( $data->data['link'] ),
 			'path'        => untrailingslashit( parse_url( get_permalink( $data->data['ID'] ) )['path'] )
@@ -175,14 +205,12 @@ class REST extends Factory {
 					'terms'    => $item->term_id
 				]
 			],
-			/*'orderby'        => 'date',
-			'order'          => "DESC",*/
 			'posts_per_page' => get_option( 'posts_per_page' )
 		] );
 
 		global $wp_taxonomies;
 		if ( isset( $wp_taxonomies[ $item->taxonomy ] ) ) {
-			$data->data[ 'post_type' ] = $wp_taxonomies[ $item->taxonomy ]->object_type[0];
+			$data->data['post_type'] = $wp_taxonomies[ $item->taxonomy ]->object_type[0];
 		}
 
 		return $data;
@@ -201,16 +229,14 @@ class REST extends Factory {
 	 *
 	 * @since   0.0.1
 	 */
-	static function prepare_post_type_archive( $data, $item, $request ){
+	static function prepare_post_type_archive( $data, $item, $request ) {
 
 		/** @var \WP_REST_Request $params */
 		$params = $request->get_params();
 
 		static::query( $data->data, [
 			'post_type'      => $params['type'],
-			'posts_per_page' => get_option( 'posts_per_page' ),
-			/*'orderby'        => 'date',
-			'order'          => "DESC"*/
+			'posts_per_page' => get_option( 'posts_per_page' )
 		] );
 
 		return $data;
@@ -228,7 +254,7 @@ class REST extends Factory {
 	 *
 	 * @since   0.0.1
 	 */
-	static function query( &$_data, $args ){
+	static function query( &$_data, $args ) {
 
 		/** @var \WP_Query $query */
 		$query = new \WP_Query( $args );
@@ -236,11 +262,12 @@ class REST extends Factory {
 		if ( ! $query->have_posts() ) {
 
 			wp_reset_postdata();
+
 			return $_data;
 
 		} else {
 
-			$_data['post_count'] = $query->post_count;
+			$_data['post_count']  = $query->post_count;
 			$_data['found_posts'] = (int) $query->found_posts;
 
 			foreach ( $query->posts as $p ) {
@@ -251,10 +278,12 @@ class REST extends Factory {
 					'id'             => $p->ID,
 					'type'           => $p->post_type,
 					'title'          => $p->post_title,
-					'excerpt'        => $p->post_excerpt,
+					'excerpt'        => always_excerpt( $p->post_content, $p->post_excerpt ),
 					'content'        => $p->post_content,
+					'date'           => date( static::$date_format, strtotime( $p->post_date ) ),
 					'featured_media' => [
 						'thumbnail' => $attachment_id,
+						'medium'    => $attachment_id,
 						'large'     => $attachment_id,
 					],
 					'route'          => [
@@ -266,12 +295,14 @@ class REST extends Factory {
 					],
 				];
 
-				if( has_post_thumbnail( $p->ID ) ) {
+				if ( has_post_thumbnail( $p->ID ) ) {
 					$featured_image     = wp_get_attachment_image_url( $attachment_id, 'thumbnail' );
+					$medium_image       = wp_get_attachment_image_url( $attachment_id, 'medium' );
 					$featured_image_lrg = wp_get_attachment_image_url( $attachment_id, 'large' );
 
 					$post['featured_media']['thumbnail'] = $featured_image;
-					$post['featured_media']['large'] = $featured_image_lrg;
+					$post['featured_media']['medium']    = $medium_image;
+					$post['featured_media']['large']     = $featured_image_lrg;
 				}
 
 				$_data['posts'][] = $post;
@@ -281,5 +312,18 @@ class REST extends Factory {
 
 		return $_data;
 
+	}
+
+	public static function rest_endpoints() {
+		register_rest_route( self::API_NAMESPACE, '/user/register', [
+			[
+				'methods'             => \WP_REST_Server::READABLE,
+				'callback'            => array( __CLASS__, 'user_register' )
+			]
+		] );
+	}
+
+	public static function user_register( \WP_REST_Request $request ) {
+		$params       = $request->get_params();
 	}
 }
