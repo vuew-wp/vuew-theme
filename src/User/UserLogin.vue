98fce808a3c0a209a535d09d49e2c5a31d560872
Jared Rethman <>
2018-05-28 17:46:54 +0200

    Added User Form components. Initial commits, still WIP

---
+++ src/User/UserLogin.vue
@@ -0,0 +1,24 @@
+<template>
+    <form>
+
+    </form>
+</template>
+
+<script>
+
+    export default {
+
+        components: {},
+
+        props:[],
+
+        computed:{},
+
+        data(){
+            return {
+                title: 'Loading...',
+            }
+        }
+
+    };
+</script>
---
+++ src/User/UserRegistration.vue
@@ -0,0 +1,51 @@
+<template>
+    <vw-form :form-data="formData"></vw-form>
+</template>
+
+<script>
+
+    import {HTTP_Vuew} from '../common/http_proxy';
+
+    /**
+     * Forms
+     */
+    import VwForm from "../Form/Form";
+    import userFormJSON from '../Form/user-registration.json';
+
+    export default {
+
+        components: {
+            VwForm
+        },
+
+        props: [],
+
+        mounted() {
+            console.log(userFormJSON);
+        },
+
+        methods: {
+            submit( data ) {
+
+                console.log(data);
+
+                const vm = this;
+
+                HTTP_Vuew.get( 'user/register', {
+                    params: {
+                        'poo': 'pooop'
+                    }
+                }).then(res => {
+                    console.log(res.data);
+                });
+            }
+        },
+
+        data() {
+            return {
+                formData: userFormJSON
+            }
+        }
+
+    };
+</script>
