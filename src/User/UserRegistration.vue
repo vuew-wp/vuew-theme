1204805c49f830c90aca4f19eb26d92818ae7841
Jared Rethman <>
2018-05-31 09:32:13 +0200

    cleaned up user-registration form

--- src/User/UserRegistration.vue
+++ src/User/UserRegistration.vue
@@ -1,7 +1,3 @@
-<template>
-    <vw-form :form-data="formData"></vw-form>
-</template>
-
 <script>
 
     import {HTTP_Vuew} from '../common/http_proxy';
@@ -9,32 +5,17 @@
     /**
      * Forms
      */
-    import VwForm from "../Form/Form";
+    import Form from "../Form/Form";
     import userFormJSON from '../Form/user-registration.json';
 
     export default {
 
-        components: {
-            VwForm
-        },
-
-        props: [],
-
-        mounted() {
-            console.log(userFormJSON);
-        },
+        extends: Form,
 
         methods: {
             submit( data ) {
-
-                console.log(data);
-
-                const vm = this;
-
                 HTTP_Vuew.get( 'user/register', {
-                    params: {
-                        'poo': 'pooop'
-                    }
+                    params: data
                 }).then(res => {
                     console.log(res.data);
                 });
