715c066154bfc6c6ff409f84499c4cf503a500af
Jared Rethman <>
2018-06-12 21:24:21 +0200

    Reverted back to script loading of css

--- header.php
+++ header.php
@@ -30,9 +30,9 @@
 
 <body <?php body_class(); ?>>
 
-<!--https://github.com/filamentgroup/loadCSS/-->
+<!--https://github.com/filamentgroup/loadCSS/>
 <link rel="preload" href="https://fonts.googleapis.com/css?family=Noto+Sans" as="style" onload="this.onload=null;this.rel='stylesheet'">
-<link rel="preload" href="<?php echo get_theme_file_uri( 'dist/main.css' ); ?>" as="style" onload="this.onload=null;this.rel='stylesheet'">
+<link rel="preload" href="<?php /*echo get_theme_file_uri( 'dist/main.css' ); */?>" as="style" onload="this.onload=null;this.rel='stylesheet'">-->
 
 <div style="position: absolute; top: 0; width: 100%; z-index: 0; font-family: Noto Sans,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;font-size: 16px;
     font-weight: 400;
