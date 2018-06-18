5ddbdab6960a36d968172e5254d58ad2f3100820
Jared Rethman <>
2018-06-18 17:08:47 +0200

    WIP tweaking initial loads and above the fold testing - part 2

--- src/Partials/Loop/LoopAboveTheFold.vue
+++ src/Partials/Loop/LoopAboveTheFold.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="( post, index ) in queriedObject.posts">
-            <attachment v-if="index < 1" :src="post.featured_media.thumbnail" :type="'background'" style="padding-bottom: 80%" class="uk-width-1-1 uk-position-relative">
+            <attachment v-if="index < 1" :src="post.featured_media.thumbnail" :type="'background'" style="padding-bottom: 80%; background-color: #444;" class="uk-width-1-1 uk-position-relative">
                 <div style="position: absolute;bottom: 0;padding: 20px; color: #eaeaea; background-color: rgba(0,0,0,0.7); right: 0; left: 0;">
                     <h2 v-html="post.title" style="color: #ffffff; font-size: 2.5rem;"></h2>
                 </div>
