60bf4feb90f416b81ae94d1ef7f6491f1255143b
Jared Rethman <>
2018-06-29 21:00:33 +0200

    Added GA

--- footer.php
+++ footer.php
@@ -40,10 +40,19 @@
 
 <script defer>
     //<![CDATA[
-	<?php if( ! WP_DEBUG ) : ?>
+    <?php if( ! WP_DEBUG ) : ?>
         <?php echo file_get_contents( get_theme_file_path( 'dist/vendor.js' ) ); ?>
         <?php echo file_get_contents( get_theme_file_path( 'dist/main.js' ) ); ?>
-	<?php endif; ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121620171-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-121620171-1');
+    </script>
+<?php endif; ?>
     //]]>
     /*if ('serviceWorker' in navigator) {
 		window.addEventListener('load', function() {
