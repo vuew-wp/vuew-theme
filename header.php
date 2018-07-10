806eb63dbeae5e8b675c687fee4f0570f0975d16
Jared Rethman <>
2018-07-10 17:34:42 +0200

    Simplified header

--- header.php
+++ header.php
@@ -6,25 +6,27 @@
 
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
+    <head>
 
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<?php // @todo make dynamic ?>
-    <title>VUEW</title>
-    <meta name="Description" content="A VueJS and WordPress Theme">
+		<?php // @todo make dynamic ?>
+        <title>VUEW</title>
+        <meta name="Description" content="A VueJS and WordPress Theme">
 
-    <link rel="manifest" href="<?php echo get_theme_file_uri('manifest.json'); ?>">
-    <meta name="theme-color" content="#15a9d1"/>
+        <link rel="manifest" href="<?php echo get_theme_file_uri( 'manifest.json' ); ?>">
+        <meta name="theme-color" content="#15a9d1"/>
 
-	<?php wp_head(); ?>
+		<?php wp_head(); ?>
 
-    <style>
-        <?php echo file_get_contents( get_theme_file_path('dist/main.css') ); ?>
-    </style>
+        <style>
+            <?php if( ! WP_DEBUG ) : ?>
+                <?php echo file_get_contents( get_theme_file_path('dist/main.css') ); ?>
+            <?php endif; ?>
+        </style>
 
-</head>
+    </head>
 
 <body <?php body_class(); ?>>
-<?php //get_template_part( 'partials/header.php' );
+<?php
